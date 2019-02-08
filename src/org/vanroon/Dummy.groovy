f54aec94975241f33192cc3b5217d509acd41a4d
Erik van Roon <evanroon.contractor@libertyglobal.com>
2019-02-08 13:45:40 +0100

    k

--- src/org/vanroon/Dummy.groovy
+++ src/org/vanroon/Dummy.groovy
@@ -32,10 +32,6 @@ class Dummy extends PipelineBuilder {
         steps.echo "infos"
         for ( i in infos ){
             def person = infos[i]
-//            def env = envs[envName]
-//            def environment = env['parameters']['environment']
-//            def city = person['House_params']['City']
-            steps.echo city
         }
 
     }
@@ -44,13 +40,9 @@ class Dummy extends PipelineBuilder {
         jEcho "Hi, my name is: " + name
     }
 
-//    def getUser() {
-//        def userId = findUser()
-//        jEcho userId
-//    }
-
     def getUser() {
-        return getBuildUser()
+        echo(getBuildUser())
+
     }
 
 }
