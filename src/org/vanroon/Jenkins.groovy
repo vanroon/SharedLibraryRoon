721de037292af0c79aaa5172b80be00c57e55ce4
Erik van Roon <erik.van.roon@devoteam.com>
2019-06-17 15:34:56 +0200

    delete job method

--- src/org/vanroon/Jenkins.groovy
+++ src/org/vanroon/Jenkins.groovy
@@ -39,7 +39,7 @@ class Jenkins extends PipelineBuilder {
             conn.setRequestProperty("Authorization", "Basic ${auth}")
         }
        conn.setRequestProperty("Jenkins-Crumb", jenkinsCrumb)
-       jEcho conn.responseCode
+       jEcho conn.responseCode.toString()
 
     }
 
