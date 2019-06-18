e495ce2fac662832eaf3f8aa78f4606dd681ecdb
Erik van Roon <erik.van.roon@devoteam.com>
2019-06-18 09:10:05 +0200

    fix
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix data send in body request
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix
    
    fix

--- src/org/vanroon/Jenkins.groovy
+++ src/org/vanroon/Jenkins.groovy
@@ -8,6 +8,7 @@ class Jenkins extends PipelineBuilder {
 
     def url
     def credentials
+    def workspace
 
     Jenkins(steps, url, credentials){
         super(steps)
@@ -15,99 +16,70 @@ class Jenkins extends PipelineBuilder {
         this.credentials = credentials
     }
 
+    def setJenkinsWorkspace(workspace){
+        this.workspace = workspace
+    }
+
     def getCrumb(userColonPass=""){
-        // steps.withCredentials (
-        //     [steps.usernameColonPassword(credentialsId: credentials, variable: 'CREDENTIALS')]
-        // ) {
-            //String creds = CREDENTIALS
-            def resultText = getHttpRequestJson(url + "/crumbIssuer/api/json", userColonPass)
-            def result = steps.readJSON text: resultText
-            //def crumb = result['crumb']
-            //return crumb
-            return result['crumb']
-
-        //}
+        def resultText = getHttpRequestJson(url + "/crumbIssuer/api/json", userColonPass)
+        def result = steps.readJSON text: resultText
+        return result['crumb']
     }
 
-    def deleteJob(String userColonPass="", String jobName){
-        def jenkinsCrumb = getCrumb(userColonPass)
-        jEcho jenkinsCrumb
+    def deleteJob(String userColonPass, String jobName){
         def delUrl = url + "/job/${jobName}/doDelete"
-        jEcho delUrl
-        def conn = new URL(delUrl).openConnection() as HttpURLConnection
-        conn.setRequestMethod("POST")
-        if (userColonPass.length() > 0){
-            def auth = encodeBase64(userColonPass)
-            conn.setRequestProperty("Authorization", "Basic ${auth}")
+        def jenkinsCrumb = getCrumb(userColonPass)
+        def auth = encodeBase64(userColonPass)
+        jEcho "Deleting job: ${jobName}"
+        def result = postHttpRequestWithXMLBody(delUrl, jenkinsCrumb, auth)
+
+        if (result == 200 || result == 201) {
+            jEcho "HTTP Status code: " + result.toString()
+            jEcho "job deleted"
+        } else if (result == 400) {
+            jEcho "HTTP Status code: " + result.toString()
+            jEcho "Bad Request... Does the job exist?"
+        } else if (result == 403) {
+            jEcho "HTTP Status code: " + result.toString()
+            jEcho "Code = 403, but job is deleted."
+        }    
+        else {
+            jEcho "HTTP Status code: " + result.toString()
+            jEcho "other error"
         }
-       conn.setRequestProperty("Jenkins-Crumb", jenkinsCrumb)
-       jEcho conn.getResponseCode().toString()
 
-    }
-
-
-// FINIS THIS..
-    def addPipelineJob(String userColonPass="", String jobName, ){
-        // first get file
-        pipelineConfig = new URL('http://10.212.0.109:8082/dwg-overhead/dwg-automation/raw/master/apiCalls/jenkins/addPipelineJob/pipelineConfig.xml').getText()
-        // second replace values
-        pipelineConfig = pipelineConfig.replaceAll("@@gitlabconnection@@", "${GITLAB_CONN}")
-        pipelineConfig = pipelineConfig.replaceAll("@@secrettoken@@", "${SECRET_TOKEN}")
-        pipelineConfig = pipelineConfig.replaceAll("@@gitrepo@@", "${GIT_REPO}")
-        pipelineConfig = pipelineConfig.replaceAll("@@credentialsid@@", "${CREDENTIALS_ID}")
-        pipelineConfig = pipelineConfig.replaceAll("@@scriptPath@@", "${SCRIPT_PATH}")
+    def addPipelineJob(
+        String userColonPass="", 
+        String jobName, 
+        String gitlabRepository,
+        String gitlabConnection,
+        String gitlabToken,
+        String gitlabCredentialsId,
+        String scriptPath="Jenkinsfile"
+    ){
+        def pipelineConfig = new URL('http://10.212.0.109:8082/dwg-overhead/dwg-automation/raw/master/apiCalls/jenkins/addPipelineJob/pipelineConfig.xml').getText('UTF-8')
+        pipelineConfig = pipelineConfig.replaceAll("@@gitlabconnection@@", "${gitlabConnection}")
+        pipelineConfig = pipelineConfig.replaceAll("@@secrettoken@@", "${gitlabToken}")
+        pipelineConfig = pipelineConfig.replaceAll("@@gitrepo@@", "${gitlabRepository}")
+        pipelineConfig = pipelineConfig.replaceAll("@@credentialsid@@", "${gitlabCredentialsId}")
+        pipelineConfig = pipelineConfig.replaceAll("@@scriptPath@@", "${scriptPath}")
         
-        // theDir = new File(steps.envVars.get('WORKSPACE'))
-        // println theDir.exists()
-        // def config = new File('config.xml')
-        // config.write "this is some content" //<< new URL("https://raw.githubusercontent.com/vanroon/misc/master/jenkins/actions/config-default.xml").getText()
-         sh("ls -als && pwd && cat config.xml")
-
-        // thirt exec post command
+        // Get Jenkins crumb
+        def jenkinsCrumb = getCrumb(userColonPass)
+        // Process authentication
+        def auth = encodeBase64(userColonPass)
+        // Pass request
+        def result = postHttpRequestWithXMLBody(url + "/createItem?name=${jobName}", jenkinsCrumb, auth, pipelineConfig)
+      
+        if (result == 200 || result == 201) {
+            jEcho result.toString()
+            jEcho "job created"
+        } else if (result == 400) {
+            jEcho result.toString()
+            jEcho "Bad Request.. Does the job already exist?"
+        } else {
+            jEcho result.toString()
+            jEcho "other error"
+        }
     }
-
-    // def getOldWay(){
-    //     steps.withCredentials (
-    //         [steps.usernameColonPassword(credentialsId: credentials, variable: 'USERPASS')]
-    //     ){
-    //         def searchUrl = url + "/crumbIssuer/api/json"
-    //         def resultText = sh ([script: """ curl -s -v -u\${USERPASS} -X GET '${searchUrl}'""", returnStdout: true]).trim()
-    //         def result = steps.readJSON text: resultText
-    //         return result['crumb']
-    //     }
-    // }
-
-    // def getCrumb2(userColonPass="") {
-    //     // steps.withCredentials (
-    //     //     [steps.usernameColonPassword(credentialsId: credentials, variable: 'creds')]
-    //     // ){
-    //         // return url
-    //         // jEcho url
-    //         // jEcho credentials.length().toString()
-    //         // jEcho "${env.creds}"
-    //         def conn = new URL(url).openConnection() as HttpURLConnection
-    //         if (userColonPass.length() > 0){
-    //             def auth = encodeBase64(userColonPass)
-    //             conn.setRequestProperty("Authorization", "Basic ${auth}")
-    //         }
-    //         if (conn.responseCode == 200){
-    //             // def json = conn.inputStream.withCloseable { inStream ->
-    //             //     new JsonSlurper().parse( inStream as InputStream )
-    //             // }
-    //             // return json
-    //             def resultText = conn.getInputStream().getText()
-    //         //def result = steps.readJSON text: resultText
-    //             return resultText
-    //         /// return result['crumb']
-    //         }
-        
-    //         // def getRC = get.getResponseCode()
-    //         // //return getRC
-    //         // //println(getRC);
-    //         // if(getRC.equals(200)) {
-    //         //     return get.getInputStream().getText()
-    //         // }
-    //    // }
-    // }
-
 }
