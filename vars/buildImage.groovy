74f43a84992fbc4a7eb41741a5fa3e66d91086ff
Erik van Roon <hfvanroon@gmail.com>
2020-12-01 14:16:51 +0100

    add file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,16 +3,28 @@
 import com.company.GlobalVars
 
 /**
- * buildImage: builds image
+ * buildImage: builds image and pushes to registry
  * INPUT:
- *  - String imageTag
  * OUTPUT:
- *  - 
  */
 
 
-def call(String imageTag) {
+def call(String BuildTemplate) {
     timestamps {
+        openshift.withCluster() {
+            openshift.withProject(GlobalVars.BUILD_PROJECT) {
+                def build = openshift.apply(openshift.process( BuildTemplate, \
+                    "-p", "APP_NAME=$APP_NAME", \
+                    "-p", "GIT_SOURCE_URL=$GIT_SOURCE_URL", \
+                    "-p", "GIT_SOURCE_REF=$GIT_REF", \
+                    "-p", "BUILDER_IMAGE_TYPE=$BUILDER_IMAGE_TYPE", \
+                    "-p", "GIT_SOURCE_SECRET=$GIT_SOURCE_SECRET", \
+                    "-p", "GIT_COMMIT_SHA=$GIT_COMMIT"
+                ))
+                // def created = openshift.apply( msbuild )
+                def bc = build.startBuild("--follow")
+            }
+        }
         script {
           sh '''
             echo oiIamAStepBuildingIMAGE
